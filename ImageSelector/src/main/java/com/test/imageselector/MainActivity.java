9450d05b0a6edec50480583b7332d23687873ee7
sam <872102569@qq.com>
2017-11-23 15:15:42 +0800

    add image filter

--- ImageSelector/src/main/java/com/test/imageselector/MainActivity.java
+++ ImageSelector/src/main/java/com/test/imageselector/MainActivity.java
@@ -12,6 +12,7 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,7 +43,7 @@ public class MainActivity extends AppCompatActivity
 
     private GridView gv_image;
     private List<String> mImages;
-    private imageAdapter mImgAdapter;
+    private ImageAdapter mImgAdapter;
     private RelativeLayout rl_imgDirSelect;
     private TextView tv_imgDir;
     private TextView tv_imgCount;
@@ -72,8 +73,21 @@ public class MainActivity extends AppCompatActivity
             Toast.makeText(this, "未扫描到任何图片", Toast.LENGTH_SHORT).show();
             return;
         }
-        mImages = Arrays.asList(mCurrentDir.list());
-        mImgAdapter = new imageAdapter(this, mImages, mCurrentDir.getAbsolutePath());
+        mImages = Arrays.asList(mCurrentDir.list(new FilenameFilter()
+        {
+            @Override
+            public boolean accept(File dir, String name)
+            {
+                if (name.endsWith(".jpg") || name.endsWith(".jpeg")
+                        || name.endsWith(".png")||name.endsWith(".JPG") || name.endsWith(".JPEG")
+                        || name.endsWith(".PNG"))
+                {
+                    return true;
+                }
+                return false;
+            }
+        }));
+        mImgAdapter = new ImageAdapter(this, mImages, mCurrentDir.getAbsolutePath());
         gv_image.setAdapter(mImgAdapter);
         tv_imgDir.setText(mCurrentDir.getName());
         tv_imgCount.setText(mMaxCount + "");
@@ -95,19 +109,21 @@ public class MainActivity extends AppCompatActivity
         rl_imgDirSelect = (RelativeLayout) findViewById(R.id.rl_imgDirSelect);
         tv_imgDir = (TextView) findViewById(R.id.tv_imgDir);
         tv_imgCount = (TextView) findViewById(R.id.tv_imgCount);
-        final View image_dir_popup = getLayoutInflater().inflate(R.layout.image_dir_popup,null);
+        final View image_dir_popup = getLayoutInflater().inflate(R.layout.image_dir_popup, null);
         ListView lv_imgDir = (ListView) image_dir_popup.findViewById(R.id.lv_imgDir);
         lv_imgDir.setAdapter(new ImgDirAdapter(mFolderBeans));
-        final PopupWindow imageDirPopupWindow = new PopupWindow(image_dir_popup, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, true);
+        final PopupWindow imageDirPopupWindow = new PopupWindow(image_dir_popup, ViewGroup.LayoutParams.MATCH_PARENT,
+                600, true);
         imageDirPopupWindow.setOutsideTouchable(true);
+        imageDirPopupWindow.setFocusable(true);
         imageDirPopupWindow.setBackgroundDrawable(new BitmapDrawable());
         rl_imgDirSelect.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                Log.e("y","y");
-                imageDirPopupWindow.showAsDropDown(rl_imgDirSelect);
+                Log.e("y", "y");
+                imageDirPopupWindow.showAtLocation(rl_imgDirSelect, Gravity.TOP, 0, 0);
             }
         });
         lv_imgDir.setOnItemClickListener(new AdapterView.OnItemClickListener()
@@ -116,6 +132,26 @@ public class MainActivity extends AppCompatActivity
             public void onItemClick(AdapterView<?> parent, View view, int position, long id)
             {
                 imageDirPopupWindow.dismiss();
+                mCurrentDir = new File(mFolderBeans.get(position).getDir());
+                mImages = Arrays.asList(mCurrentDir.list(new FilenameFilter()
+                {
+                    @Override
+                    public boolean accept(File dir, String name)
+                    {
+                        if (name.endsWith(".jpg") || name.endsWith(".jpeg")
+                                || name.endsWith(".png") || name.endsWith(".JPG") || name.endsWith(".JPEG")
+                                || name.endsWith(".PNG"))
+                        {
+                            return true;
+                        }
+                        return false;
+                    }
+                }));
+                mImgAdapter = new ImageAdapter(MainActivity.this, mImages, mCurrentDir.getAbsolutePath());
+                gv_image.setAdapter(mImgAdapter);
+                tv_imgDir.setText(mCurrentDir.getName());
+                tv_imgCount.setText(mImages.size() + "");
+                Log.e("dirpath", mCurrentDir.getAbsolutePath());
             }
         });
     }
@@ -136,8 +172,8 @@ public class MainActivity extends AppCompatActivity
                 Uri imgUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
                 ContentResolver contentResolver = getContentResolver();
                 Cursor cursor = contentResolver.query(imgUri, null,
-                        MediaStore.Images.Media.MIME_TYPE + " = ? or " + MediaStore.Images.Media.MIME_TYPE + " = ?",
-                        new String[]{"image/png", "image/jpeg"}, MediaStore.Images.Media.DATE_MODIFIED);
+                        MediaStore.Images.Media.MIME_TYPE + " = ? or " + MediaStore.Images.Media.MIME_TYPE + " = ? or " + MediaStore.Images.Media.MIME_TYPE + " = ? ",
+                        new String[]{"image/png", "image/jpg", "image/jpeg"}, MediaStore.Images.Media.DATE_MODIFIED);
 
                 //存储已扫描的父文件路径
                 Set<String> mDirPaths = new HashSet<String>();
@@ -172,7 +208,8 @@ public class MainActivity extends AppCompatActivity
                         public boolean accept(File dir, String name)
                         {
                             if (name.endsWith(".jpg") || name.endsWith(".jpeg")
-                                    || name.endsWith(".png"))
+                                    || name.endsWith(".png")||name.endsWith(".JPG") || name.endsWith(".JPEG")
+                                    || name.endsWith(".PNG"))
                             {
                                 return true;
                             }
@@ -195,14 +232,14 @@ public class MainActivity extends AppCompatActivity
         }.start();
     }
 
-    private class imageAdapter extends BaseAdapter
+    private class ImageAdapter extends BaseAdapter
     {
 
         private String mDirPath;
         private List<String> mImgPaths;
         private LayoutInflater mInflater;
 
-        public imageAdapter(Context context, List<String> mDatas, String dirPath)
+        public ImageAdapter(Context context, List<String> mDatas, String dirPath)
         {
             this.mDirPath = dirPath;
             this.mImgPaths = mDatas;
@@ -278,8 +315,8 @@ public class MainActivity extends AppCompatActivity
             View view;
             if (convertView == null)
             {
-                view = getLayoutInflater().inflate(R.layout.item_image_dir_popup,parent,false);
-            }else
+                view = getLayoutInflater().inflate(R.layout.item_image_dir_popup, parent, false);
+            } else
             {
                 view = convertView;
             }
@@ -289,7 +326,7 @@ public class MainActivity extends AppCompatActivity
             FolderBean folderBean = folderBeanList.get(position);
             Glide.with(MainActivity.this).load(folderBean.getFirstImgPath()).into(iv_imgDir);
             tv_dirName.setText(folderBean.getName());
-            tv_dirCount.setText(folderBean.getCount());
+            tv_dirCount.setText(folderBean.getCount() + "");
             return view;
         }
     }
